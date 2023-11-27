4ae2d1013c6652c9c81eea8dc1b6845959154315
Mrnidhi <139426377+Mrnidhi@users.noreply.github.com>
2023-11-28 00:19:40 +0530

    Add files via upload

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -10,8 +10,43 @@ module.exports.signup = (req, res) => {
     newUser.save().then(() => {
         res.send({ code : 200, message: 'Signup success'})
     }).catch((err) => {
-         res.send({ code: 500, message: 'Signup Err'})
+         res.send({ code: 500, message: 'Signup Er'})
             })
+
+
+    
+}
+module.exports.login = (req, res) => {
+    console.log(req.body) 
+    
+    // email and password match
+
+    UserModel.findOne({ email: req.body.email })
+        .then(result=> {
+            console.log(result, '11')
+
+        // match password with req.body.password
+             if(result.password === req.body.password) {
+                 res.send({ code: 404, message : 'password wrong'})
+             } else {
+                 res.send({ 
+                    code: 200, 
+                    message : 'user Found',
+                    token: 'fafgshu'
+
+            })
+        }
+
+        })
+        .catch( err => {
+             res.send({ code: 500, message: 'user not found'})
+    })
+
+    // newUser.save().then(() => {
+    //     res.send({ code : 200, message: 'Login success'})
+    // }).catch((err) => {
+    //      res.send({ code: 500, message: 'Login Err'})
+    //         })
 
 
     
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userController = require('./controller/user');
 
 const port = 5000;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+// Use the cors middleware
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('DB connected');
   })
@@ -21,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/signup', userController.signup);
+app.post('/login', userController.login);
 
 app.listen(port, () => {
   console.log(`Backend Running at port ${port}`);
